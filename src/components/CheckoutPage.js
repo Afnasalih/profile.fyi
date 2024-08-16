3e642fe369ce59cf3cf9dba03090ba653326e59d
Afnasalih <91935925+Afnasalih@users.noreply.github.com>
2024-08-17 01:26:58 +0530

    Update CheckoutPage.js

--- src/components/CheckoutPage.js
+++ src/components/CheckoutPage.js
@@ -1,17 +1,11 @@
 'use client';
 
-import React, { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import React from 'react';
+
 
 export default function CheckoutPage() {
-  const router = useRouter();
 
-  // useEffect(() => {
-  //   const isLoggedIn = localStorage.getItem('isLoggedIn');
-  //   if (!isLoggedIn) {
-  //     router.push('/login');
-  //   }
-  // }, [router]);
+
 
   return (
     <div className="text-center p-6">
